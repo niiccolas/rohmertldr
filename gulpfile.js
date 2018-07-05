236df9e02062b0e9d5dd836f07223e8ce6e85cc4
niiccolas <niiccolas@protonmail.com>
2018-07-05 23:49:34 +0200

    Add Gulp build config sending minified css/js/html to ./dist

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp');
+var autoprefixer = require ('gulp-autoprefixer');
+var csso = require('gulp-csso');
+var del = require('del');
+var htmlmin = require('gulp-htmlmin');
+var concat = require('gulp-concat');
+var composer = require('gulp-uglify/composer');
+var uglifyjs = require('uglify-es');
+var minify = composer(uglifyjs, console);
+
+// Supported browsers
+const AUTOPREFIX_BROWSERS = [
+  'ie >= 10',
+  'ie_mob >= 10',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 7',
+  'opera >= 23',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10',
+];
+
+// TASK: Minify CSS
+gulp.task('styles', function () {
+  return gulp.src('./app/assets/css/**/*.css')
+    // Auto-prefix for cross-browser compatibility
+    .pipe(concat('styles.css'))
+    .pipe(autoprefixer({browsers: AUTOPREFIX_BROWSERS}))
+    // Minify the file
+    .pipe(csso())
+    // Output
+    .pipe(gulp.dest('./dist/assets/css/'))
+  });
+
+  // TASK: Concat and minify JS
+  gulp.task('scripts', function (){
+    var options = {};
+    return gulp.src('./app/assets/js/**/*.js')
+      .pipe(concat('app.js'))
+      .pipe(minify(options))
+      .pipe(gulp.dest('./dist/assets/js'));
+  });
+
+// TASK: Minify HTML files
+gulp.task('pages', function (){
+  return gulp.src('./app/**/*.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true
+    }))
+    .pipe(gulp.dest('./dist'));
+})
+
+// Delete current ./dist folder
+gulp.task('clean', () => del(['dist']));
+
+gulp.task('default', gulp.series('clean', 'styles', 'scripts', 'pages', (done) => {
+  console.log('****** FRESH ./dist FOLDER EXPORTED ******');
+  done();
+}));
