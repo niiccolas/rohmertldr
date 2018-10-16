d3d27bbcbbfbbb550007c335abbae72fe42d005b
niiccolas <40167702+niiccolas@users.noreply.github.com>
2018-10-16 19:06:42 +0200

    Add scroll to top function

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -6,6 +6,24 @@ const toggleSound = () => {
   return document.getElementById('mute').children[0].textContent === 'volume_up' ? document.getElementById('mute').children[0].textContent = 'volume_off' : document.getElementById('mute').children[0].textContent = 'volume_up'; // toggle reference of material icons
 };
 
+// Smooth top scrolling function
+// https://stackoverflow.com/questions/21474678/scrolltop-animation-without-jquery#24559613
+function scrollToTop(scrollDuration) {
+  const scrollHeight = window.scrollY;
+  const scrollStep = Math.PI / ( scrollDuration / 15 );
+  const cosParameter = scrollHeight / 2;
+  let scrollCount = 0;
+  let scrollMargin;
+  let scrollInterval = setInterval(function () {
+    if (window.scrollY != 0) {
+      scrollCount = scrollCount + 1;
+      scrollMargin = cosParameter - cosParameter * Math.cos(scrollCount * scrollStep);
+      window.scrollTo(0, (scrollHeight - scrollMargin));
+    }
+    else clearInterval(scrollInterval);
+  }, 15);
+}
+
 // Populate footer with current year
 document.querySelector('#currentYear').textContent = new Date().getFullYear();
 
@@ -38,7 +56,7 @@ const thumbnails = document.querySelectorAll('img')
 const getThumbFilename = function(e) {
   const filename = e.target.src.match(/[^/]+$/)[0].slice(0,-4); // get the filename from the src url
   loadVideoToDOM(filename);
-  window.scrollTo(0,0);
+  scrollToTop(300);
 };
 thumbnails.forEach(x => x.addEventListener('click', getThumbFilename));
 
